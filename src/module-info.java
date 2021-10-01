feb14fac74a6cbe4e3c1749180b8d5e130c71d68
Kamil Kacprzak <Kamil.Kacprzak@yahoo.com>
2021-10-01 19:49:49 +0100

    Basic functionality

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Kamil
+ *
+ */
+module sorty {
+}
---
+++ src/sorty/Main.java
@@ -0,0 +1,25 @@
+package sorty;
+
+import java.util.Scanner;
+
+class Main {
+
+	private final static Scanner in = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		Menu menu = new Menu(in);
+		String exitStr;
+		//Main, infinity loop of a program, which shows console menu,
+		//until user provide exit character (Y)
+		do {
+			menu.open();
+			System.out.println("Would you like to exit now? Y/N");
+			exitStr = in.nextLine();
+			if(exitStr.toUpperCase().equals("Y")) {
+				break;
+			}
+		}while(true);
+		
+		in.close();
+	}
+}
